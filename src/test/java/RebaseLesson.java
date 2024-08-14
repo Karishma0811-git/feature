832eac1c3b781671d227f0c86d9491a9e6ba0059
Karishma0811-git <arorakarishma0811@gmail.com>
2024-08-14 09:55:58 +0530

    Demo for rebase

--- src/test/java/RebaseLesson.java
+++ src/test/java/RebaseLesson.java
@@ -1,5 +1,6 @@
 public class RebaseLesson {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("This demo is for git rebase from intellij");
     }
 }
